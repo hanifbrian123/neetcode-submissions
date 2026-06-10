35b60b6ae29b8208569aec7ccef1c5da62ac5de7
hanifbrian123 <brianhanif12345@gmail.com>
2026-06-10 12:40:17 +0700

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freqTrue = {chr(i):0 for i in range(97, 123)}
+        for s in s1: freqTrue[s]+=1
+        
+        freqWindow = {chr(i):0 for i in range(97, 123)}
+        l, r, n = 0, 0, len(s2)
+        while r < n:
+            currS2 = s2[r]
+            freqWindow[currS2] += 1
+            incrementR = True
+            if freqWindow == freqTrue:
+                return True
+            elif not freqTrue[currS2]: # tidak ada char ini di freq true, artinya r ini sudah tidak valid lagi dan l maupun r baru bisa dipindah ke r+1, freqwindow bisa kembali di kosongkan lagi
+                l = r+1
+                r = l
+                incrementR = False
+                freqWindow = {chr(i):0 for i in range(97, 123)}
+            elif freqWindow[currS2] > freqTrue[currS2]:
+                while freqWindow[currS2] > freqTrue[currS2]:
+                    freqWindow[s2[l]] -= 1
+                    l+=1
+            if incrementR:
+                r+=1
+        return False
