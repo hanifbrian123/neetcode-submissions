2c4b0ac514ebde0d8301f2c6c464cf2770074b1f
hanifbrian123 <brianhanif12345@gmail.com>
2026-08-13 09:35:32 +0700

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def isValidT(a, b):
+            cntFalse = 0
+            for i in range(len(a)):
+                if a[i] != b[i]:
+                    cntFalse+=1
+            return cntFalse == 1
+
+        q = collections.deque([beginWord])
+        visited = {beginWord}
+        lv = 1
+        while q:
+            node = q.popleft()
+            if node == endWord:
+                return lv
+            for w in wordList:
+                if isValidT(node, w) and w not in visited:
+                    visited.add(w)
+                    q.append(w)
+            lv+=1
+        return 0
+
+
+            
+
