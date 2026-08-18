0733f41642e1d26876b28505a91d2c16ed7c71d3
hanifbrian123 <brianhanif12345@gmail.com>
2026-08-18 10:59:07 +0700

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        self.nums = nums
+        return max(nums[0], self.bottomUp(0,n-1), self.bottomUp(1,n))
+
+    def bottomUp(self, s, e):
+        max1, max2 = 0, 0
+        for i in range(s, e):
+            temp = max(max1, max2)
+            max1 = max2+self.nums[i]
+            max2 = temp
+        print(max1, max2)
+        return max(max1, max2)
