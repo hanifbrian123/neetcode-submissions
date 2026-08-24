44e920da9bfc6e7823fceda0c10e97f351594479
hanifbrian123 <brianhanif12345@gmail.com>
2026-08-24 07:38:43 +0700

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1 for i in range(amount+1)]
+        dp[amount] = 0
+        def dfs(cur):
+            if cur > amount: return 10005
+            elif dp[cur]!=-1: return dp[cur]
+
+            best = 10005
+            for c in coins:
+                best = min(best, dfs(cur+c))
+            dp[cur] = 1+best
+            return 1+best
+        res = dfs(0)
+        return res if res<=10000 else -1
