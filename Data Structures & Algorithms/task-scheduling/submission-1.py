17e1137b766c6b88f261d42cce1cfcab4bdac3db
hanifbrian123 <brianhanif12345@gmail.com>
2026-09-22 08:31:11 +0700

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        cnt = {}
+        for ch in tasks:
+            cnt[ch] = 1 + cnt.get(ch, 0)
+        
+        heap = [(-c, ch) for ch, c in cnt.items()]
+        heapq.heapify(heap)
+        q = collections.deque([])
+        tasksLeft = len(tasks)
+        i = 0
+        while tasksLeft > 0:
+            if heap:
+                c, ch = heapq.heappop(heap)
+                cnt[ch] -= 1
+                tasksLeft -= 1
+                if cnt[ch]:
+                    q.append([ch, i+n+1])
+                # print(f"{ch} {i}")
+            elif q and i >= q[0][1]:
+                ch = q.popleft()[0]
+                cnt[ch] -= 1
+                tasksLeft -= 1
+                if cnt[ch]:
+                    q.append([ch, i+n+1])
+                # print(f"{ch} {i}")
+                # print(f"IDLE {i}")
+            
+            i+=1
+        return i
+
+
+
+# a: 1
+# b: 1
+# c: 1
+
+# a -> b -> c ->idl-> a -> b -> c ->idl-> a
+# 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8
+# q = [a, 4] | q = [b, 5], [c, 6]
+
+
+# if i == q.peek[1]: taruh q.peek[0]
+# else if maxheap.length: taruh max_heap.pop
+# else: taruh idle
